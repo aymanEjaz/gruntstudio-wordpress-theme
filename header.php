3c65a357149a8927e86db9dbd32bf489dfbf2bfb
aymanijaz <aymanijaz21@gmail.com>
2021-05-17 01:37:51 +0500

    added get header/footer and body_class

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Grunt &mdash; Website Template by Colorlib</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <?php wp_head(); ?>
+    
+  </head>
+
+  <body data-spy="scroll" data-target=".site-navbar-target" data-offset="300" <?php body_class(); ?>>
+  
+
+
+  <div class="site-wrap">
